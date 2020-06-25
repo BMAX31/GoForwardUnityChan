2e25d3fc07db50fc6b8c991dcbf98d8a426d0848
水田明良 <kiriboshi31@gmail.com>
2020-06-25 19:04:10 +0900

    GoForwardUnityChanプロジェクト　初めてのコミット

---
+++ Assets/HitPlaySound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour
+{
+
+    public AudioClip sound;
+
+    void OnCollisionEnter2D(Collision2D Collison)
+    {
+        if (Collison.gameObject.tag != "UnityChanTag")
+        {
+            GetComponent<AudioSource>().Play();
+            //AudioSource.PlayClipAtPoint(sound, transform.position);
+        }
+    }
+}
